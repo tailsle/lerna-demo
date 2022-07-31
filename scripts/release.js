2a70017140b9a860313a414f1781ddf980ed425e
lee <408718358@qq.com>
2022-07-31 22:52:22 +0800

    init

---
+++ scripts/release.js
@@ -0,0 +1,48 @@
+(async () => {
+  const glob = (await import("glob")).default;
+  const inquirer = (await import("inquirer")).default;
+  const fs = (await import("fs-extra")).default;
+  const handlebars = (await import("handlebars")).default;
+  const { execaSync } = await import("execa");
+  const path = await import("path");
+  const cwd = process.cwd();
+
+  const getPackagePath = () => {
+    const packagePaths = glob.sync(path.join(process.cwd(), "packages/*"));
+    console.log({ packagePaths });
+    return packagePaths.map((item) => item.replace(process.cwd() + "/", ""));
+  };
+
+  const choosePackage = async (packages) => {
+    console.log(packages);
+    const answer = await inquirer.prompt({
+      type: "checkbox",
+      name: "packages",
+      message: "选择你要发布的包",
+      choices: [...packages],
+    });
+    return answer;
+  };
+
+  const reWriteLerna = (packages) => {
+    const jsonContent = fs.readFileSync(`${cwd}/lerna-template.txt`, "utf-8");
+    const jsonResult = handlebars.compile(jsonContent)(packages);
+    fs.writeFileSync(`${cwd}/lerna.json`, jsonResult);
+  };
+
+  const publish = async () => {
+    const packages = getPackagePath();
+    const publishPackages = await choosePackage(packages);
+    if (publishPackages.packages.length !== 0) {
+      reWriteLerna(publishPackages);
+      execaSync("lerna publish", {
+        stdio: "inherit",
+        cwd,
+      });
+    } else {
+      console.log("没有选择包");
+    }
+  };
+
+  publish();
+})();
